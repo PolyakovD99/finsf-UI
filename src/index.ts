a6e1f7f4cbd43ee560cabe6f979461384dc0f25f
Denis Polyakov <polyakovd@finsfera.ua>
2024-03-22 13:12:29 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-export * from "./components";
-
-export { default as useAutocomplete } from "@mui/material/useAutocomplete";
-export { default as GlobalStyles } from "@mui/material/GlobalStyles";
-export * from "@mui/material/GlobalStyles";
-export { unstable_composeClasses } from "@mui/base/composeClasses";
-export { default as generateUtilityClass } from "@mui/material/generateUtilityClass";
-export * from "@mui/material/generateUtilityClass";
-export { default as generateUtilityClasses } from "@mui/material/generateUtilityClasses";
-export { default as Unstable_TrapFocus } from "@mui/material/Unstable_TrapFocus";
-export { default as ThemeProvider } from "@mui/material/styles";
-export { default as CustomTheme } from "./styles/theme/theme";
+export { default as theme } from "./theme";
